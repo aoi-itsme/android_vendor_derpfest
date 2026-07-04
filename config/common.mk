6dfb0c5da41aaf749bab6d24c638af3cc3427275
LuK1337 <priv.luk@gmail.com>
2026-07-04 15:13:37 +0200

    config: Exclude prebuilts/misc/protobuf_vendorcompat
    Change-Id: Ibbe03b9be35ab2b44a57e22f702564e833f6feb4

--- config/common.mk
+++ config/common.mk
@@ -2,8 +2,9 @@
 $(call inherit-product-if-exists, vendor/extra/product.mk)
 $(call inherit-product-if-exists, vendor/lineage/config/derpfest.mk)
 
-# Exclude kernel platform repos from bp scanning
+# Exclude repos from bp scanning
 PRODUCT_SOURCE_ROOT_DIRS += -kernel/platform
+PRODUCT_SOURCE_ROOT_DIRS += -prebuilts/misc/protobuf_vendorcompat
 
 # Allow vendor prebuilt repos to exclude themselves from bp scanning
 -include $(sort $(wildcard vendor/*/*/exclude-bp.mk))
